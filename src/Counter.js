17528e74cb0447e1429bf2c67727c1db079e3d15
Feltesse <er.feltesse@gmail.com>
2020-05-03 20:12:11 +0200

    additionnal input

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleChange(event){
+    const newCount = event.target.value
+    newCount===''?setCount(0):setCount(parseInt(newCount))
+  }
+
+  return (
+    <div>
+      <input type='text 'onClick={handleChange} value={count}/>
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
