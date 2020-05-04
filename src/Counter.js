c13d85e28f5e010825ac3b776db133860bdbc793
Feltesse <er.feltesse@gmail.com>
2020-05-04 09:32:19 +0200

    résolution onChange

--- src/Counter.js
+++ src/Counter.js
@@ -5,12 +5,12 @@ function Counter() {
 
   function handleChange(event){
     const newCount = event.target.value
-    newCount===''?setCount(0):setCount(parseInt(newCount))
+    newCount===""?setCount(0):setCount(parseInt(newCount))
   }
 
   return (
     <div>
-      <input type='text 'onClick={handleChange} value={count}/>
+      <input type='text 'onChange={handleChange} value={count}/>
       <p>Le compteur est  à : {count} </p>
       <button onClick={() => setCount(count + 1)}>
         +1
